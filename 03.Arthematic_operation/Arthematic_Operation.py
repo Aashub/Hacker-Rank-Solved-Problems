b283e8562cce399a4fc8b343203dc7b1f47dd8a0
Aashu Bamniya <ashishbam07@gmail.com>
2024-11-12 18:41:52 +0530

    initial commit

---
+++ 03.Arthematic_operation/Arthematic_Operation.py
@@ -0,0 +1,14 @@
+if __name__ == '__main__':
+    # here we are giving an input in a and b and storing them in variable
+    a = int(input())
+    b = int(input())
+    
+    # here arthematic operation is being performed and result is being stored in variable
+    sum = a + b
+    sub = a - b
+    mul = a * b
+    
+    print(sum)
+    print(sub)
+    print(mul)
+    
